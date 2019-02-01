2d5d91be20a74804b0d11a05e3fa664e08d791e6
Jiri Vanek <jvanek@redhat.com>
2019-02-01 09:49:46 +0100

    Chancged main package

--- src/org/fbb/board/internals/PhotoGrider.java
+++ src/org/fbb/board/internals/PhotoGrider.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package photogrider;
+package org.fbb.board.internals;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
