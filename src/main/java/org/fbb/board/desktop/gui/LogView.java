18a75cea212424aa722014fc02fa7e61abe601cf
Jiri <jvanek@redhat.com>
2019-03-18 18:17:15 +0100

    LogView now have autorefresh

--- src/main/java/org/fbb/board/desktop/gui/LogView.java
+++ src/main/java/org/fbb/board/desktop/gui/LogView.java
@@ -10,7 +10,10 @@ import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -24,6 +27,38 @@ import org.fbb.board.internals.GuiLogHelper;
  */
 public class LogView extends JFrame {
 
+    private class Autorefrsh extends Thread {
+
+        private boolean alive = true;
+        private final JTextArea o;
+        private final JTextArea e;
+        private final JCheckBox r;
+
+        private Autorefrsh(JTextArea o, JTextArea e, JCheckBox r) {
+            this.o = o;
+            this.e = e;
+            this.r = r;
+        }
+
+        @Override
+        public void run() {
+            while (alive) {
+                try {
+                    if (r.isSelected()) {
+                        e.setText(GuiLogHelper.guiLogger.getSerr());
+                        e.setCaretPosition(e.getDocument().getLength());
+                        o.setText(GuiLogHelper.guiLogger.getSout());
+                        o.setCaretPosition(o.getDocument().getLength());
+                    }
+                    Thread.sleep(500);
+                } catch (Exception ex) {
+                    GuiLogHelper.guiLogger.loge(ex);
+                }
+            }
+        }
+
+    }
+
     public LogView() throws HeadlessException {
         this.setLayout(new BorderLayout());
         JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
@@ -34,7 +69,7 @@ public class LogView extends JFrame {
         split.add(new JScrollPane(o));
         split.add(new JScrollPane(e));
         this.add(split);
-        JPanel tools = new JPanel(new GridLayout(1, 2));
+        JPanel tools = new JPanel(new GridLayout(1, 3));
         JButton or = new JButton("refresh");
         or.addActionListener(new ActionListener() {
 
@@ -44,6 +79,8 @@ public class LogView extends JFrame {
             }
         });
         tools.add(or);
+        JCheckBox autor = new JCheckBox("autorefresh", true);
+        tools.add(autor);
         JButton er = new JButton("refresh");
         er.addActionListener(new ActionListener() {
 
@@ -58,6 +95,21 @@ public class LogView extends JFrame {
         split.setDividerLocation(400);
         this.setSize(800, 800);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        final Autorefrsh autorefresh = new Autorefrsh(o, e, autor);
+        this.addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+                try {
+                    super.windowClosed(e);
+                } finally {
+                    autorefresh.alive = false;
+                }
+
+            }
+
+        });
+        autorefresh.start();
 
     }
 
