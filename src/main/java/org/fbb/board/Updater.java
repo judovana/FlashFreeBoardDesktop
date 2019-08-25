992f013cc40d3eb5bcec3927867b99adb7505aa7
Jiri <jvanek@redhat.com>
2019-08-25 13:29:28 +0200

    Implemented backend for autoupdating

---
+++ src/main/java/org/fbb/board/Updater.java
@@ -0,0 +1,237 @@
+package org.fbb.board;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.StandardCharsets;
+import org.fbb.board.internals.GuiLogHelper;
+
+/**
+ *
+ * @author jvanek
+ */
+public class Updater {
+
+    private static final String FILE_NAME = "latest-release";
+    private static final String exUrl = "https://raw.githubusercontent.com/judovana/FlashFreeBoardDesktop/master/src/main/resources/org/fbb/board" + FILE_NAME;
+    private static final String inPath = "/org/fbb/board/" + FILE_NAME;
+
+    private static URL getExURL() throws MalformedURLException {
+        return new URL(exUrl);
+    }
+
+    private static URL getInURL() throws MalformedURLException {
+        return Updater.class.getResource(inPath);
+    }
+
+    private static URL getCachedURL() throws MalformedURLException {
+        return getCacheFile().toURI().toURL();
+    }
+
+    private static File getCacheFile() throws MalformedURLException {
+        return new File(System.getProperty("java.io.tmpdir"), FILE_NAME);
+    }
+
+    private static String readFromUrl(URL u) throws IOException {
+        String r = "";
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(u.openStream(), StandardCharsets.UTF_8))) {
+            while (true) {
+                String s = in.readLine();
+                if (s == null) {
+                    break;
+                }
+                s = s.trim();
+                if (s.startsWith("#")) {
+                    continue;
+                }
+                r = r + "" + s;
+            }
+        }
+        return r;
+    }
+
+    private static void cache(String items) throws IOException {
+        save(getCacheFile(), items);
+    }
+
+    private static void save(File f, String s) throws IOException {
+        try (BufferedWriter in = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f), StandardCharsets.UTF_8))) {
+            in.write(s);
+        }
+
+    }
+
+    private static enum LoadedUrlState {
+
+        NETWORK, CACHE, LOCAL, FATALTY;
+
+        private String getResolution() {
+            switch (this) {
+                case NETWORK:
+                    return "Ok!";
+                case CACHE:
+                    return "Error!";
+                case LOCAL:
+                    return "Error!";
+                case FATALTY:
+                    return "Error!";
+            }
+            return "Unknown Error explaining state";
+        }
+    }
+
+    private static class RemoteUrlWithStatus {
+
+        private final URL r;
+        private final LoadedUrlState w;
+
+        private RemoteUrlWithStatus(String r, LoadedUrlState w) throws MalformedURLException {
+            this.r = new URL(r);
+            this.w = w;
+        }
+
+        public String getResult() {
+            return r.toExternalForm();
+        }
+
+        public URL getUrl() {
+            return r;
+        }
+
+        public LoadedUrlState getSource() {
+            return w;
+        }
+
+        @Override
+        public String toString() {
+            return w + ": " + r;
+        }
+
+    }
+
+    private static RemoteUrlWithStatus obtain() throws MalformedURLException {
+        String r;
+        //network
+        try {
+            r = readFromUrl(getExURL());
+            if (r != null && !r.isEmpty()) {
+                try {
+                    //save it
+                    cache(r);
+                    GuiLogHelper.guiLogger.logo("Cache updated sucesfully. " + getCacheFile());
+                } catch (Exception ex) {
+                    GuiLogHelper.guiLogger.loge(ex);
+                }
+                GuiLogHelper.guiLogger.logo("Loaded remote file. " + getExURL());
+                return new RemoteUrlWithStatus(r, LoadedUrlState.NETWORK);
+            }
+        } catch (Exception ex) {
+            GuiLogHelper.guiLogger.loge(ex);
+        }
+        //cache
+        try {
+            r = readFromUrl(getCachedURL());
+            if (r != null && !r.isEmpty()) {
+                GuiLogHelper.guiLogger.logo("Read from cache: " + getCacheFile());
+                return new RemoteUrlWithStatus(r, LoadedUrlState.CACHE);
+            }
+        } catch (Exception ex) {
+            GuiLogHelper.guiLogger.loge(ex);
+        }
+
+        try {
+            r = readFromUrl(getInURL());
+            if (r != null && !r.isEmpty()) {
+                GuiLogHelper.guiLogger.logo("Read from app: " + getInURL());
+                return new RemoteUrlWithStatus(r, LoadedUrlState.LOCAL);
+            }
+        } catch (Exception ex) {
+            GuiLogHelper.guiLogger.loge(ex);
+        }
+        GuiLogHelper.guiLogger.loge("All ways failed");
+        return new RemoteUrlWithStatus("http://nohting.found/error", LoadedUrlState.FATALTY);
+    }
+
+    private static String checkForCurrent() {
+        if (Updater.class.getClassLoader() instanceof URLClassLoader) {
+            URLClassLoader tHis = (URLClassLoader) Updater.class.getClassLoader();
+            URL[] cp = tHis.getURLs();
+            for (URL jar : cp) {
+                if (jar.getFile().matches(".*FlashFreeBoardDesktop.*\\.jar")) {
+                    return jar.getFile();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void downloadUsingNIO(String urlStr, String file) throws IOException {
+        URL url = new URL(urlStr);
+        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        fos.close();
+        rbc.close();
+    }
+
+    public static class Update {
+
+        public final URL from;
+        public final File to;
+
+        public Update(URL from, File to) {
+            this.from = from;
+            this.to = to;
+        }
+
+    }
+
+    public static Update getUpdatePossibility() {
+        try {
+            RemoteUrlWithStatus r = obtain();
+            if (r.w == LoadedUrlState.NETWORK) {
+                String current = checkForCurrent();
+                if (current != null) {
+                    File fold = new File(current);
+                    String nwName = new File(r.getUrl().getFile()).getName();
+                    GuiLogHelper.guiLogger.logo("From: " + fold.getName() + "(" + fold.getAbsolutePath() + ")");
+                    GuiLogHelper.guiLogger.logo("To  : " + nwName + " (" + r.getUrl().toExternalForm() + ")");
+                    if (fold.getName().equals(nwName)) {
+                        return null;
+                    }
+                    return new Update(r.getUrl(), fold.getAbsoluteFile());
+                } else {
+                    GuiLogHelper.guiLogger.loge("Nothig to update from");
+                    return null;
+                }
+            } else {
+                GuiLogHelper.guiLogger.loge("Nothing to update to");
+                return null;
+            }
+        } catch (Exception ex) {
+            GuiLogHelper.guiLogger.loge(ex);
+            return null;
+        }
+    }
+
+    public static void main(String... args) throws MalformedURLException {
+        Update u = getUpdatePossibility();
+        if (u == null) {
+            System.out.println(Translator.R("UpdateImpossible"));
+        } else {
+            System.out.println(Translator.R("CurrentVersion", u.to.getName(), u.to.getAbsolutePath()));
+            System.out.println(Translator.R("RemoteVersion", new File(u.from.getFile()).getName(), u.from.toExternalForm()));
+        }
+
+    }
+
+}
